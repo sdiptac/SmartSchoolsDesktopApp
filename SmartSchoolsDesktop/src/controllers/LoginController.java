11e0776827b51a94b508648cfe9d7513ab1dfe1e
Dan <kramer425@gmail.com>
2016-07-06 15:29:21 -0400

    added login

--- SmartSchoolsDesktop/src/controllers/LoginController.java
+++ SmartSchoolsDesktop/src/controllers/LoginController.java
@@ -3,14 +3,21 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.AdminUser;
+import application.Connector;
 import application.Main;
+import application.PageType;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 
 public class LoginController extends PageController{
 	@FXML
 	private Label incorrectPasswordLabel;
+	
+	@FXML
+	private PasswordField passwordField;
 	
 	@Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -19,6 +26,15 @@ public class LoginController extends PageController{
 	
 	@FXML
 	protected void signInPress(Event event) {
-	     Main.getInstance().changeScene("LocationsPage");
+	     AdminUser user = Main.getInstance().getAdminUser();
+	     user.setPassword(passwordField.getPromptText());
+	     if(Connector.connect()){
+	    	 user.setSignedIn(true);
+	    	 Main.getInstance().changeScene(PageType.pageMap.get(PageType.Type.MAIN_MENU));
+	     }else{
+	    	 user.setSignedIn(false);
+	    	 user.setPassword("");
+	    	 incorrectPasswordLabel.setVisible(true);
+	     }
 	}
 }
