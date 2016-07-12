233667093386a60a860fa16104b47afcd3cbedd0
Dan <kramer425@gmail.com>
2016-07-12 10:04:50 -0400

    fixed incorrect column indexes for accesspoints

--- SmartSchoolsDesktop/src/database/CSVLocation.java
+++ SmartSchoolsDesktop/src/database/CSVLocation.java
@@ -14,7 +14,7 @@ public class CSVLocation {
 		
 		try{
 			Connector.connect();
-			PreparedStatement prepare = Connector.connection.prepareStatement("select duration,timeOfAccess,room,building,bssid,ssid,org,first_name,last_name from accessedAP natural join accesspoint natural join user order by userid");
+			PreparedStatement prepare = Connector.connection.prepareStatement("select duration,timeOfAccess,room,building,bssid,ssid,first_name,last_name from accessedAP natural join accesspoint natural join user order by userid");
 	
 			ResultSet resultset  = prepare.executeQuery();
 			if (!resultset.next()){ 
@@ -22,23 +22,22 @@ public class CSVLocation {
 			}
 			
         	do {
-        		String[] row = new String[9];
+        		String[] row = new String[8];
         		row[0] = resultset.getString("first_name");
         		row[1] = resultset.getString("last_name");
         		row[2] = resultset.getString("duration");
         		row[3] = resultset.getString("timeOfAccess");
         		row[4] = resultset.getString("room");
         		row[5] = resultset.getString("building");
-        		row[6] = resultset.getString("org");
-        		row[7] = resultset.getString("bssid");
-        		row[8] = resultset.getString("ssid");
+        		row[6] = resultset.getString("bssid");
+        		row[7] = resultset.getString("ssid");
         		info.add(row);
         	} while (resultset.next());
         	
         	Connector.disconnect();
 
 			try{
-				if(!ExportToCSV.export(absolutePath, "AccessPointData", new String[]{"First_Name", "Last_Name", "Duration", "Time of Access", "Room", "Building", "BSSID", "SSID", "Org"}, info)){
+				if(!ExportToCSV.export(absolutePath, "AccessPointData", new String[]{"First_Name", "Last_Name", "Duration", "Time of Access", "Room", "Building", "BSSID", "SSID"}, info)){
 					return "The number of columns provided does not match the number of columns in the data"; 
 				}
 			}catch(FileNotFoundException f){
